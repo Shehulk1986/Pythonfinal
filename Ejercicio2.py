f3eaea20cc73a9aad7410620ad03ad30df1a4b63
Shehulk1986 <yolimatatis781@gmail.com>
2024-11-28 10:11:29 -0500

    terminado 2

---
+++ Ejercicio2.py
@@ -0,0 +1,17 @@
+#Solicitar al usuario que ingrese el número de rango
+
+init = int(input("Ingrese el número de inicio del rango:"))
+end = int(input("Ingrese el número de fin del rango "))
+
+if init > end:
+    print("el número inicial debe ser mayor al numero final")
+    
+else:
+    imprimir_impares(init, end)
+    
+def imprimir_impares(init, end):
+    for numero in range(init, end +1):
+    if numero % 2 !=0:
+        print(numero)
+
+
