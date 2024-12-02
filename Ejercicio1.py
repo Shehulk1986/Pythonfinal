d7c91083869de8150d43f729daf7f96a4ebfbd7a
Shehulk1986 <yolimatatis781@gmail.com>
2024-12-02 08:49:12 -0500

    terminado 10

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,22 +1,17 @@
 #Solicitar al usuario que ingrese el numero
 
-def calcular_factorial(n):
-    if n < 0:
-        return "Error: El factorial no está definido para números negativos."
-    
-    elif n == 0:
-        return 1   # Return se utulizar para devolver un valor desde la funcion
-    
-    else:
-        factorial = 1
-    
-    for i in range(1,n + 1):
-        factorial *= i
+# Definir variable
+n = 0
 
 # Pedir al usuario que ingrese un número entero
 n = int(input("ingrese un numero entero:"))
 
 # Calcular y mostrar factorial
-factorial = calcular_factorial(n)
+factorial = 1
+    
+for i in range(1,n + 1):
+        factorial *= i
+
+#Mostrar el resultado
 
 print(f"El factorial de {n} es:{factorial}")    
